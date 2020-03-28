edd7c673baa78c3d1627ffd586f27a64308a8259
Ali Raza <fitnessh6@outlook.com>
2020-03-28 11:50:02 +0500

    Integrate AdminLTE 2

--- Inventory Management Systems/App_Start/BundleConfig.cs
+++ Inventory Management Systems/App_Start/BundleConfig.cs
@@ -9,19 +9,16 @@ namespace Inventory_Management_Systems
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
-                         "~/admin-lte/js/app.js")
-                         .Include("~/admin-lte/plugins/fastclick/fastclick.js"
-                         )
-                         .Include("~/Scripts/AdminLTE_dist/js/adminlte.js")
-                         );
+            bundles.Add(new ScriptBundle("~/admin-lte/js")
+             .Include("~/admin-lte/plugins/fastclick/fastclick.js"
+             
+             ));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery")
                        .Include("~/Scripts/jquery-{version}.js")
-                        .Include("~/Scripts/jquery-3.4.1.min.js")
-                        .Include("~/Content/plugins/jquery/jquery.min.js")
-                        .Include("~/Content/Magicsuggest-magicsuggest-cf74869/magicsuggest-min.js")
-                        .Include("~/Scripts/AdminLTE/jquery/jquery.js")
+                       .Include("~/admin-lte/bower_components/select2/dist/js/select2.min.js")
+                     
+                      
                         
 
                        );
@@ -29,7 +26,7 @@ namespace Inventory_Management_Systems
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                          "~/Scripts/jquery.validate*")
                         .Include("~/Scripts/jquery.validate.unobtrusive.js")
-                        .Include("~/Scripts/jquery-ui-1.12.1.min.js")
+                        
                         
                    
                        
@@ -46,25 +43,19 @@ namespace Inventory_Management_Systems
                                         
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js")
-                      .Include("~/Content/plugins/datatables-bs4/css/dataTables.bootstrap4.css")
-                      .Include("~/Scripts/AdminLTE/bootstrap/js/bootstrap.bundle.js")
+                    //.Include("~/Content/plugins/bootstrap/jsbootstrap.bundle.js")
+                    //  .Include("~/Scripts/AdminLTE/bootstrap/js/bootstrap.bundle.js")
                       
 
 
                       );
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                              "~/Content/bootstrap.css")
-                             .Include("~/Content/site.css")
-                              .Include("~/admin-lte/css/AdminLTE.css")
-                              .Include("~/admin-lte/css/skins/skin-blue.css")
-                             .Include("~/Content/Magicsuggest-magicsuggest-cf74869/magicsuggest-min.css")
-                             .Include("~/Content/plugins/fontawesome-free/css/all.css")
-                             .Include("~/Content/AdminLTE/css/adminlte.css")
-
-
-
-                              );
+                       "~/Content/bootstrap.css")
+                      .Include("~/admin-lte/bower_components/select2/dist/css/select2.min.css")
+                       .Include("~/admin-lte/css/AdminLTE.css")
+                      .Include("~/admin-lte/css/skins/skin-blue.css"
+                       ));
         }
     }
 }
